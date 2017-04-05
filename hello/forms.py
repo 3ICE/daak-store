103d70d5213b4a06ee20f577b14b78ed6cf306c1
Daniel Berezvai <daniel.berezvai@student.tut.fi>
2017-04-06 00:07:27 +0300

    Inform -> provide [English]

--- hello/forms.py
+++ hello/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, help_text='Required. Provide a valid email address.')
 
     class Meta:
         model = User
