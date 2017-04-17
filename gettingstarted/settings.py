2dca287da85cb6e60cbd6c55440cbf2bb2a282a4
Aparajita Chowdhury <chowdhua@student.tut.fi>
2017-04-18 00:06:34 +0300

    trying to fix .models error

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -21,9 +21,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 if DEBUG:
-  from forms.py import BogusSMTPConnection
-  from django.core import mail
-  mail.SMTPConnection = BogusSMTPConnection
+    from .models import BogusSMTPConnection
+    from django.core import mail
+    mail.SMTPConnection = BogusSMTPConnection
 
 # SECURITY WARNING: change this before deploying to production!
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
