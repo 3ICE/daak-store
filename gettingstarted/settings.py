a94d8f290da36edc34e0e4243335427375c720ee
Aparajita Chowdhury <chowdhua@student.tut.fi>
2017-04-19 13:10:50 +0300

    testing

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 import os
 import dj_database_url
-
+from forms.py import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -165,4 +165,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-LOGIN_REDIRECT_URL = 'profile_developer'
+if(signup.developer):
+    LOGIN_REDIRECT_URL = 'profile_developer'
+else:
+    LOGIN_REDIRECT_URL = 'player'
