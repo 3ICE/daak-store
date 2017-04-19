5bbad6fb18a8c67662138de03df2d175a65c696e
Aparajita Chowdhury <chowdhua@student.tut.fi>
2017-04-19 14:43:16 +0300

    trying to import forms

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 import os
 import dj_database_url
-from forms.py import *
+from forms import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
